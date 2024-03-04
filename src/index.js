04e2e5631823cddba78b89832bfe2a03e3cfbe53
devanshV18 <devanshverma024@gmail.com>
2024-03-04 23:45:50 +0530

    comments added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 
+//abfbainfiafn
 import connectDB from "./db/index.js";
 
 dotenv.config({path: './env'})
