fe2a4514512d9855016551ae5f8c3ae6be290c56
devanshV18 <devanshverma024@gmail.com>
2024-03-05 04:25:56 +0530

    async handler created

--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+//we are setting up configs for various channels through which our backend / server might recieve requests - from url,from frontend forms, json formats
+
+//FOr configs we use middle wares which are accessed using the .use method of express app
+
+// ----------------------------------------------------------------------------------------------------///////
+
+//configuring json limit
+app.use(express.json({limit: "16kb"}))
+
+
+
+//url encoding for requests coming from urls
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}))
+
+
+
+//when we want to store some files on server like pdfs, images etc we use a static named middle ware that creates a folder named in quotes
+app.use(express.static("public"))
+
+
+
+//to access client's browsers cookies and set the cookies
+app.use(cookieParser( ))
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,24 +1,7 @@
-const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+ const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
     }
-}
+ }
 
-
-
-
-export {asyncHandler}
-
-
-// try catch function to async :-
-
-// const asyncHandler = (fn) => async (req,res,next) => {
-//     try{
-//         await fn(req, res , next)
-//     }catch(error){
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message:err.message
-//         })
-//     }
-// }
+ export {asyncHandler}
