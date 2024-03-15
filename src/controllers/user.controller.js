29890958c08cbc44aa735d680bd29f8db809a123
devanshV18 <devanshverma024@gmail.com>
2024-03-15 19:57:48 +0530

    username and email validations are completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"  //proxy user and model
 
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend as per usermodel
@@ -16,6 +18,29 @@ const registerUser = asyncHandler( async (req, res) => {
 
     const {fullname, email, username, password} = req.body
     console.log("email: ", email);
+
+    //2 - check for all if
+    // if(fullname === ""){
+    //     throw new ApiError(400, "fullname is required")
+    // }
+
+    //or
+
+    if (
+        [fullname,email,username,password].some((fields) => fields?.trim()==="")
+    ) {
+        throw new ApiError(400,"All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username } , { email }]
+    })
+
+    if(existedUser){
+        throw new ApiError(409,"User with email or username alrteady exists")
+    }
+
+
 
 })
 
