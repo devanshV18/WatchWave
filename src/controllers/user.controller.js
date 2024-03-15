b33d6fefb95bd5879680769aa43c712662e0ca6d
devanshV18 <devanshverma024@gmail.com>
2024-03-15 21:12:10 +0530

    minor formattings

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -87,13 +87,6 @@ const registerUser = asyncHandler( async (req, res) => {
         new ApiResponse(200,createdUser, "User Registered Successfully")
      )
 
-
-
-
-
-
-
-
 })
 
 export {registerUser}
